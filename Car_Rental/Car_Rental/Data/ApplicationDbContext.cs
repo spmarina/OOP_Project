0b4e3b71b051c467fa6d4e85732030d152a57523
DaIfe59 <dmisekin@gmail.com>
2024-06-02 20:33:16 +0300

    Models

---
+++ Car_Rental/Car_Rental/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Car_Rental.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Car_Rental.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Rent> Rents { get; set; }
+        public DbSet<ServiceDate> ServicesDates { get; set; }
+    }
+}

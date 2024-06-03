d8a4f57298e9cdeb64d3938c223b0a724c41f066
DaIfe59 <dmisekin@gmail.com>
2024-06-03 21:51:14 +0300

    Cache

--- Car_Rental/Car_Rental/obj/Debug/net8.0/Car_Rental.AssemblyInfo.cs
+++ Car_Rental/Car_Rental/obj/Debug/net8.0/Car_Rental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Car_Rental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03b06473fcbe9dce06d4010266765c1aa5444b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5803c1944d240b3eb84f4a26267125251c4116d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Car_Rental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Car_Rental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

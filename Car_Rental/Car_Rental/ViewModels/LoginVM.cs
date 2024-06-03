05e9c707e3fece5924a0cb9873a1a67aaeec4523
DaIfe59 <dmisekin@gmail.com>
2024-06-03 11:23:36 +0300

    Дурацкий html + что-то

---
+++ Car_Rental/Car_Rental/ViewModels/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Car_Rental.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Введите ваш Login")]
+        [EmailAddress(ErrorMessage = "Неверный формат Login")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Введите ваш пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}

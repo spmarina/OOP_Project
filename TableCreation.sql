ad130c5f18119e01bed2411613d37f781a6c0e73
spmarina <mspiridonova2013@gmail.com>
2024-06-05 00:02:58 +0300

    bd
    исправила ТО и аренды

--- TableCreation.sql
+++ TableCreation.sql
@@ -101,64 +101,125 @@ go
 
 -------------------------------------------------------------
 
-ALTER TABLE Contracts
-ADD DocumentLink NVARCHAR(max) not null;
+--ALTER TABLE Contracts
+--ADD DocumentLink NVARCHAR(max) not null;
+--go
+
+--INSERT INTO Cars 
+--VALUES
+--('BMW', 'X7 M Sport', 14000, 1),
+--('Mercedes-Benz', 'GLS', 16500, 0),
+--('Audi', 'A7', 10000, 1)
+
+--go
+
+
+--INSERT INTO Customers 
+--VALUES
+--('Ivanov', 'Egor', 'Andreevich', +79173451789, 1),
+--('Emelyanova', 'Darya', 'Daniilovna', +79176661320, 1),
+--('Stolyarova', 'Yliya', 'Vladimirovna', +79654569302, 0)
+
+--go
+
+
+
+
+
+--INSERT INTO Cards 
+--VALUES
+--(1, 3, 400, 40000),
+--(2, 10, 1400, 140000),
+--(3, 5, 520, 52000)
+
+--go
+
+
+
+--ALTER TABLE ServiceDate
+--ALTER COLUMN PreviousDate DATE;
+--go
+
+--ALTER TABLE ServiceDate
+--ALTER COLUMN NextDate DATE;
+--go
+
+--ALTER TABLE Contracts
+--ALTER COLUMN CreateDate DATE;
+--go
+
+
+
+
+--INSERT INTO ServiceDate (Cars_ID, PreviousDate, NextDate)
+--VALUES
+--(1, '2022-05-29', '2024-11-29'),
+--(2, '2023-04-15', '2025-04-15'),
+--(3, '2022-08-24', '2024-08-14');
+
+--go
+
+--INSERT INTO Discounts 
+--VALUES
+--(1, 15),
+--(3, 5)
+
+--go
+
+--INSERT INTO Admins 
+--VALUES
+--('Michail', '12345678', 560000),
+--('Christina', 'L0veCars', 1245000)
+
+
+--go
+
+
+--INSERT INTO Contracts 
+--VALUES
+--(1, 567423, '2022-03-11', 1, 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'),
+--(2, 891023, '2023-12-06', 2, 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'),
+--(3, 459012, '2024-01-10', 3, 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
+
+--go
+
+--delete Rents;
+--go
+
+
+
+--------------------------------------------------------------------------
+
+EXEC sp_rename 'Rent', 'Rents'
+
 go
 
-INSERT INTO Cars 
-VALUES
-('BMW', 'X7 M Sport', 14000, 1),
-('Mercedes-Benz', 'GLS', 16500, 0),
-('Audi', 'A7', 10000, 1)
 
+ALTER TABLE Rents
+ALTER COLUMN FirstDate DATE;
 go
 
-
-INSERT INTO Customers 
-VALUES
-('Ivanov', 'Egor', 'Andreevich', +79173451789, 1),
-('Emelyanova', 'Darya', 'Daniilovna', +79176661320, 1),
-('Stolyarova', 'Yliya', 'Vladimirovna', +79654569302, 0)
-
+ALTER TABLE Rents
+ALTER COLUMN LastDate DATE;
 go
+
 
 
 INSERT INTO Rents 
 VALUES
-(1, 3, 29/05/2024, 01/06/2024),
-(2, 1, 01/06/2024, 03/06/2024)
-
-go
-
-
-INSERT INTO Cards 
-VALUES
-(1, 3, 400, 40000),
-(2, 10, 1400, 140000),
-(3, 5, 520, 52000)
+(1, 3, '29/05/2024', '01/06/2024'),
+(2, 1, '01/06/2024', '03/06/2024');
 
 go
 
 
 
-ALTER TABLE ServiceDate
-ALTER COLUMN PreviousDate DATE;
+EXEC sp_rename 'ServiceDate', 'ServicesDates'
+
 go
 
-ALTER TABLE ServiceDate
-ALTER COLUMN NextDate DATE;
-go
-
-
-
-ALTER TABLE Contracts
-ALTER COLUMN CreateDate DATE;
-go
-
-
-
-
-INSERT INTO ServiceDate (Cars_ID, PreviousDate, NextDate)
+INSERT INTO ServicesDates (Cars_ID, PreviousDate, NextDate)
 VALUES
 (1, '2022-05-29', '2024-11-29'),
 (2, '2023-04-15', '2025-04-15'),
@@ -166,40 +227,6 @@ VALUES
 
 go
 
-INSERT INTO Discounts 
-VALUES
-(1, 15),
-(3, 5)
+SELECT * FROM ServicesDates;
 
 go
-
-INSERT INTO Admins 
-VALUES
-('Michail', '12345678', 560000),
-('Christina', 'L0veCars', 1245000)
-
-
-go
-
-
-INSERT INTO Contracts 
-VALUES
-(1, 567423, '2022-03-11', 1, 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'),
-(2, 891023, '2023-12-06', 2, 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'),
-(3, 459012, '2024-01-10', 3, 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-
-
-go
-
-SELECT * FROM Contracts
-go
-
---delete Admins
---delete Cards
---delete Cars
---delete Contracts
---delete Customers
---delete Discounts
---delete Rents
---delete ServicesDates
---go

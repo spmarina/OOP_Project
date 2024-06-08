e72f420417894d9d21caa27aed93e2aff32ce48b
DaIfe59 <dmisekin@gmail.com>
2024-06-08 16:37:04 +0300

    Post

--- Car_Rental/Car_Rental/obj/Debug/net8.0/Car_Rental.AssemblyInfo.cs
+++ Car_Rental/Car_Rental/obj/Debug/net8.0/Car_Rental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Car_Rental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df14afa8c3548e615d7bd6e126341a92b3402832")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc36d2f97dafd9b404d53d1f1e668bd6e10778a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Car_Rental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Car_Rental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

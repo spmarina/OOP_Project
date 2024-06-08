25cbeef9493616c2f34fa7e336f8119c8ea8b7fc
NoSoul <danilasoloviev.moscow@yandex.ru>
2024-06-08 16:34:53 +0300

    new background

--- Car_Rental/Car_Rental/obj/Debug/net8.0/Car_Rental.AssemblyInfo.cs
+++ Car_Rental/Car_Rental/obj/Debug/net8.0/Car_Rental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Car_Rental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfd774deae6cc3099207bac42648c97aaa0a270f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fd5cfc188e07075ea5ef3afe1f98b1033042b50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Car_Rental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Car_Rental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

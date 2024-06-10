32656c9b1d9f16a76c20c45c555dcdf43c34db25
spmarina <mspiridonova2013@gmail.com>
2024-06-10 18:20:52 +0300

    11
    11

--- Car_Rental/Car_Rental/obj/Debug/net8.0/Car_Rental.AssemblyInfo.cs
+++ Car_Rental/Car_Rental/obj/Debug/net8.0/Car_Rental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Car_Rental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d113725fff69743df9e7340764daa66cef043573")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f1038d749e0bae692dba38a6170425fd007f95")]
 [assembly: System.Reflection.AssemblyProductAttribute("Car_Rental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Car_Rental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
